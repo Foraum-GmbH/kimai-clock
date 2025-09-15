a7bbd83c3d73d58883f70f33398a606e9f3e3ce0
undeaD_D <8116188+undeaDD@users.noreply.github.com>
2025-09-15 13:41:06 +0200

    Bump cask version to 1.0.5 for kimai-clock
    Updated version and SHA256 checksum for KimaiClock.
    
    Signed-off-by: undeaD_D <8116188+undeaDD@users.noreply.github.com>

--- Casks/kimai-clock.rb
+++ Casks/kimai-clock.rb
@@ -1,9 +1,9 @@
 cask "kimai-clock" do
-  version "1.0.2"
-  sha256 "3c9338abc428f6f10cad1bcfa580b15d6f74a14fb1003482ddc6107e93e9a5cb"
+  version "1.0.5"
+  sha256 "a54581bacc5ce909de7b5af135c49a93ddb2d2c5f0df67b7b686feb9f62c10d4"
   url "https://github.com/Foraum-GmbH/kimai-clock/releases/download/v#{version}/KimaiClock.dmg"
   name "KimaiClock"
-  desc "Track and manage Kimai 2 time entries from the menu bar"
+  desc "Track and manage Kimai timesheets from the menu bar"
   homepage "https://github.com/Foraum-GmbH/kimai-clock"
   app "KimaiClock.app"
 end
