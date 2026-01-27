80da6d6c983353805556d44ef45a7fc5fc341b28
undeaD_D <8116188+undeaDD@users.noreply.github.com>
2026-01-27 12:15:43 +0100

    Update kimai-clock to version 1.1.5
    Signed-off-by: undeaD_D <8116188+undeaDD@users.noreply.github.com>

--- Casks/kimai-clock.rb
+++ Casks/kimai-clock.rb
@@ -1,6 +1,6 @@
 cask "kimai-clock" do
-  version "1.1.4"
-  sha256 "05683ad0c8e048416d692f81d1ac893c7f0025169773866bcb80707b57ed62d0"
+  version "1.1.5"
+  sha256 "c6dc89b179a7bc146128627c6111c566ee0ba4396be308613e26c769236c3bc8"
   url "https://github.com/Foraum-GmbH/kimai-clock/releases/download/v#{version}/KimaiClock.dmg"
   name "KimaiClock"
   desc "Track and manage Kimai timesheets from the menu bar"
