155b5f3a29952191ca639c87e4b13e7a61fd9ff3
undeaD_D <8116188+undeaDD@users.noreply.github.com>
2026-03-19 20:53:24 +0100

    Update kimai-clock version to 1.5.1
    Signed-off-by: undeaD_D <8116188+undeaDD@users.noreply.github.com>

--- Casks/kimai-clock.rb
+++ Casks/kimai-clock.rb
@@ -1,5 +1,5 @@
 cask "kimai-clock" do
-  version "1.1.5"
+  version "1.5.1"
   sha256 "04b5d1bf192cdc412a5bb69e93e0c11361ba9f737cd6b551875420c365a1a4b6"
   url "https://github.com/Foraum-GmbH/kimai-clock/releases/download/v#{version}/KimaiClock.dmg"
   name "KimaiClock"
